66de3a741c23e71cb8df0d46f3926e0086ca4d45
CaroMusangi1 <kitongacarol8@gmail.com>
2024-06-02 15:46:27 +0000

    add somefile

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -58,7 +58,6 @@ class DBStorage:
     def save(self):
         """commit all changes of the current database session"""
         self.__session.commit()
-
     def delete(self, obj=None):
         """delete from the current database session obj if not None"""
         if obj is not None:
